eacd879393c45df277eb76c79b998f3a2ac07f82
Mahesh <deshmukhmahesh009@gmail.com>
2022-02-08 06:15:30 +0530

    favicon added

--- docsite-js/sidebar.js
+++ docsite-js/sidebar.js
@@ -5,16 +5,16 @@ const closeButton = document.querySelector("#close-btn");
 
 menuIconButton.addEventListener("click", () => {
   sidebar.classList.add("show-sidebar");
+  document.body.style.overflow = "hidden";
 });
 closeButton.addEventListener("click", () => {
   sidebar.classList.remove("show-sidebar");
+  document.body.style.overflow = "visible";
 });
 
 window.addEventListener("click", event => {
-  if (
-    (event.target === this.document.body || event.target === main) &&
-    this.innerWidth < 800
-  ) {
+  if ((event.target === this.document.body || event.target === main) && this.innerWidth < 800) {
     sidebar.classList.remove("show-sidebar");
+    document.body.style.overflow = "visible";
   }
 });

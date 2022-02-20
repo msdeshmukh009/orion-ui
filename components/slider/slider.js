21452d8b7dab28e95816d2ae7e3b6de6ad6715eb
Mahesh <deshmukhmahesh009@gmail.com>
2022-02-20 21:20:36 +0530

    fix: slider

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -1,9 +1,15 @@
-const slider = document.querySelector(".slider");
+const sliders = document.querySelectorAll(".slider");
 
-slider.addEventListener(
-  "input",
-  () =>
-    (slider.style.background = `linear-gradient(90deg,
-    var(--primary-color) ${slider.value}%,
-    var(--slider-bg-color) ${slider.value}%`)
-);
+function sliderOperation() {
+  const updatedColor = `linear-gradient(
+      90deg,
+      var(--primary-color) ${this.value}%,
+      var(--slider-bg-color) ${this.value}%
+  )`;
+
+  this.style.background = updatedColor;
+}
+
+sliders.forEach(slider => {
+  slider.addEventListener("input", sliderOperation);
+});
